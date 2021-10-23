bdf005065fdd88c84f340df5b1c6734e28d13e28
Thiagomdr3 <thiago_mdr3@hotmail.com>
2021-10-22 23:55:13 -0300

    Criado menu e tratativas de erros na main

---
+++ C#/Selecione_Calc_Jogo_ClassifAnimais/Selecione_Calc_Jogo_ClassifAnimais/Calculos/Calcular.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selecione_Calc_Jogo_ClassifAnimais.Calculos
+{
+    class Calcular
+    {
+        public static void SelecionarCalc()
+        {
+            Console.Clear();
+            Console.WriteLine("Você selecionou calcular!" +
+                "\n\nVamos lá!! Selecione uma das opções abaixo:\n" +
+                "\n1. Tabuada de um numero especifico;" +
+                "\n2. Tabuada de todos os numeros do 1 ao 10;" +
+                "\n3. Calcular o fatorial de um numero;" +
+                "\n4. Voltar.");
+
+            int i  = int.Parse(Console.ReadLine());
+
+            
+        }
+    }
+}
---
+++ C#/Selecione_Calc_Jogo_ClassifAnimais/Selecione_Calc_Jogo_ClassifAnimais/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Selecione_Calc_Jogo_ClassifAnimais.Calculos;
+
+namespace Selecione_Calc_Jogo_ClassifAnimais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sair = 1;
+
+            while (sair == 1)
+            {
+                Console.Clear();
+                Console.WriteLine("Bem Vindo!!!" +
+                "\n\nEscolha uma das opções abaixo:\n" +
+                "\n1. Calcular;" +
+                "\n2. Jogar;" +
+                "\n3. Classificar Animais");
+
+                int selecao = 0;
+
+                try
+                {
+                    selecao = int.Parse(Console.ReadLine());
+                }
+
+                catch
+                {
+                    Console.WriteLine("\nVocê não digitou um valor numérico. Digite um valor válido:\n");
+                }
+
+                switch (selecao)
+                {
+                    case 1:
+                        Console.Clear();
+                        Calcular.SelecionarCalc();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        break;
+                    default:
+                        
+                        while (true)
+                        {
+                            int opcao = 0;
+                            Console.WriteLine("Você digitou um valor invalido!!" +
+                            "\nTentar novamente? (1 = Sim ___ 2 = Não");
+                            try
+                            {
+                                opcao = int.Parse(Console.ReadLine());
+                            }
+                            catch
+                            {
+                                Console.WriteLine("\nVocê nao digitou um valor numérico. Digite um valor válido:\n");
+                            }
+                            if (opcao == 1 || opcao == 2)
+                            {
+                                if (opcao == 2)
+                                {
+                                    sair = 0;
+                                    Console.WriteLine("Fim do código");
+                                    break;
+                                }
+                                else if(opcao == 1)
+                                {
+                                    break;
+                                }
+
+                                
+                            }
+                        }
+                        break;
+
+                }
+            }
+        }
+    }
+}
