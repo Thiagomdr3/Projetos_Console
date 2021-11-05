027926c4ea6329a138fe2b6ca162ae3cbe729dd4
Thiagomdr3 <thiago_mdr3@hotmail.com>
2021-11-04 22:24:03 -0300

    update

---
+++ C#/ControleEStoque/App/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using ControleEStoque.src;
+using static System.Console;
+
+namespace ControleEStoque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int qntddItens;
+            List<Estoque> estoque = new();
+            Console.WriteLine("Quantos itens deseja inserir no estoque?");
+            qntddItens = int.Parse(Console.ReadLine());
+
+            
+            estoque =  Estoque.NewItem(qntddItens);
+
+            foreach(Estoque iten in estoque)
+            {
+                Console.WriteLine("id = {0} - item = {1} - quantidade = {2}", iten._id, iten._item, iten._quantidade);
+            }
+        }
+    }
+}
---
+++ C#/ControleEStoque/src/Estoque.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleEStoque.src
+{
+    public class Estoque
+    {
+
+
+        public int _id { set; get; }
+        public string _item { set; get; }
+        public int _quantidade { set; get; }
+
+        //public List<Estoque> estoques { get; set; }
+
+
+
+        public static List<Estoque> NewItem(int qntddItens)
+        {
+
+            List<Estoque> lista = new();       
+
+            for (int i = 0; i < qntddItens; i++)
+            {
+                Estoque item = new();
+                Console.WriteLine("Informe o item:");
+                item._id = new Random().Next(1, 150);
+                item._item = Console.ReadLine();
+                Console.WriteLine("Informe a quantidade:");
+                item._quantidade = int.Parse(Console.ReadLine());
+
+                lista.Add(item);
+                
+
+            }
+
+            return lista;
+        }
+
+
+    }
+
+}
